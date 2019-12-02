5a570a223676a0d633e999786444ad38c736031b
kbm1611 <57904456+kbm1611@users.noreply.github.com>
2019-12-02 15:30:24 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "main.h" 
 
 int main() {
-	int timekey, Adnumber;// Adnumber => 예매번호
-	int datekey2;
+	int timekey1_1, timekey2_2, timekey3_3;
+
+	int ticketnumber;
+	int movie_key;
+	int count;
+	int Adnumber;// Adnumber => 예매번호
+
 	int price = 0;
 
+	FILE *fp1, *fp2, *fp3, *fp4, *fp5, *fp6;
+
 	movieinformation();
+
 
 	while (1)
 	{
@@ -15,42 +23,45 @@ int main() {
 
 		system("cls");
 
-		int moviekey = movieselect(menucode);
+		moviekey = movieselect(menucode);
 		system("cls");
+
 
 		if (menucode == 0) { //예매하기
 			if (moviekey == 1) // 선택한 영화가 겨울왕국일때
 			{
-				int datecode = dateprint(menucode);
-				price = reservation(&datecode, &timekey, &datekey2, moviekey, &price); // 시간,날짜,영화 키를 받아 옴
+				price = reservation1(&timekey1_1);
 			}
 			if (moviekey == 2) // 선택한 영화가 블랙머니일때
 			{
-				int datecode = dateprint(menucode);
-				price = reservation(&datecode, &timekey, &datekey2, moviekey, &price); // 시간,날짜,영화 키를 받아 옴
+				price = reservation2(&timekey2_2);
 			}
 			if (moviekey == 3) // 선택한 영화가 신의한수일때
 			{
-				int datecode = dateprint(menucode);
-				price = reservation(&datecode, &timekey, &datekey2, moviekey, &price); // 시간,날짜,영화 키를 받아 옴
+				price = reservation3(&timekey3_3);
 			}
+
+			fp6 = fopen("count.txt", "r");
+				fscanf(fp6, "%d", &count);
+				fclose(fp6);
+				count += 1;
+
+				fp6 = fopen("count.txt", "w");
+				fprintf(fp6, "%d", count);
+				fclose(fp6);
 		}
 
 		if (menucode == 1) {   //티켓예매  출력
-			setColor(yellow, black);
-			printf("예매번호를 입력해 주십시오.(ex2019111601):");
-			setColor(lightgray, black);
+			fp5 = fopen("TicketNumber.txt", "r");
+			fscanf(fp5, "%d", &ticketnumber);
+			fclose(fp5);
+
+			printf("예매번호를 입력해 주십시오.(ex2019111601):");;
 			scanf("%d", &Adnumber);
-			if (Adnumber == datekey2 * 100 + 1)
-				Ticket(moviekey, timekey, datekey2, price);
-			else {
-
-				setColor(lightred, black);
-				printf("\n예매번호가 잘못되었습니다. 처음 화면으로 돌아갑니다.");
-				setColor(lightgray, black);
+			if (Adnumber == ticketnumber)
+			{
+				Ticket();
 			}
-			_sleep(1000);
-
 		}
 		if (menucode == 2) {  //종료
 			system("cls");
